e82bbb4831fe23b801543fdf76a1e878d1b1f324
Caio Felix <caiohalcsik@gmail.com>
2022-02-12 18:30:46 -0300

    feat: add gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
 
   useEffect(() => {
-    const handleRouteChange = (url: string) => {
+    const handleRouteChange = (url: URL) => {
       gtag.pageview(url)
     }
     router.events.on('routeChangeComplete', handleRouteChange)
